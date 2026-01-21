1303f6eb974a938f6019d7cb60d0a239a2d525bd
Connor Garey <connorgarey1999@gmail.com>
2026-01-21 20:13:03 +0000

    Added logo properties and configuration files.

--- modules/fastfetch.nix
+++ modules/fastfetch.nix
@@ -21,17 +21,78 @@ in
         '';
       };
       
-      logo-source = lib.mkOption {
-        type = lib.types.str;
-        default = "auto";
-        description = ''
-          Name of logo to display. use \"fastfetch --list-logos\" to see them all.
-        '';
+      logo = {
+        width = lib.mkOption {
+          type = lib.types.int;
+          default = 65;
+          description = ''
+            Width of the logo.
+          '';
+        };
+        
+        height = lib.mkOption {
+          type = lib.types.int;
+          default = 35;
+          description = ''
+            Height of the logo.
+          '';
+        };
+
+        source = lib.mkOption {
+          type = lib.types.str;
+          default = "auto";
+          description = ''
+            Name of logo to display. use \"fastfetch --list-logos\" to see them all.
+          '';
+        };
+
+        padding = {
+
+          top = lib.mkOption {
+            type = lib.types.int;
+            default = 0;
+            description = ''
+              Logo top padding.
+            '';
+          };
+          
+          left = lib.mkOption {
+            type = lib.types.int;
+            default = 0;
+            description = ''
+              Logo left padding.
+            '';
+          };
+          
+          right = lib.mkOption {
+            type = lib.types.int;
+            default = 2;
+            description = ''
+              Logo right padding.
+            '';
+          };
+        };
       };
     };
   };
 
   config = lib.mkIf cfg.enable {
     environment.systemPackages = [pkgs.fastfetch];
+
+    environment.etc."fastfetch/config.jsonc".text = ''
+      // Generated file.
+
+      "logo": {
+        "type": "auto",
+        "source": "${cfg.logo.source}",
+        "width": "${cfg.logo.width}",
+        "height": "${cfg.logo.height}",
+        "padding": {
+          "top": "${cfg.logo.padding.top}",
+          "left": "${cfg.logo.padding.left}",
+          "right": "${cfg.logo.padding.right}",
+        },
+      }
+    '';
   };
 }
