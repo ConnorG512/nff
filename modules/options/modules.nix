c2292e39bfb95084df5e9ee44c31c024ab6f43f5
Connor Garey <connorgarey1999@gmail.com>
2026-01-23 14:30:27 +0000

    Added modules options.

---
+++ modules/options/modules.nix
@@ -0,0 +1,17 @@
+{
+  lib,
+  ...
+}:
+
+let
+  defaultValues = import ../default-values.nix;
+in 
+{
+  options.programs.nff.settings = {
+    modules = lib.mkOption {
+      type = lib.types.listOf lib.types.str;
+      default = defaultValues.modules;
+      description = "List of modules.";
+    };
+  };
+}
